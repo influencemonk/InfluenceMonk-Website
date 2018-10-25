c4c7bf0339e4fa3962117f90d3e0f6b852d366d5
Sagnik Paul <sagnik.paul@nowfloats.com>
2018-10-26 02:11:55 +0530

    UI Changes

--- wp-content/themes/influencemonk/footer.php
+++ wp-content/themes/influencemonk/footer.php
@@ -1,19 +1,17 @@
 <div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 title">
-                <p><span>influence</span>Monk</p>
-                <p>&copy; 2018, InfluenceMonk Pvt Ltd</p>
-            </div>
-            <div class="col-xs-12 col-sm-6 social-icons">
-                <span>Follow us around the web</span>
-                <span>
-                    <img src="<?php echo get_template_directory_uri() ?>/img/facebook.svg" />
-                    <img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" />
-                    <img src="<?php echo get_template_directory_uri() ?>/img/twitter.svg" />
-                    <img src="<?php echo get_template_directory_uri() ?>/img/linkedin.svg" />
-                </span>
-            </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 title">
+            <p><span>influence</span>Monk</p>
+            <p>&copy; 2018, InfluenceMonk Pvt Ltd</p>
+        </div>
+        <div class="col-xs-12 col-sm-8 social-icons">
+            <span>Follow us around the web</span>
+            <span>
+                <img src="<?php echo get_template_directory_uri() ?>/img/facebook_logo.png" />
+                <img src="<?php echo get_template_directory_uri() ?>/img/instagram_logo.png" />
+                <img src="<?php echo get_template_directory_uri() ?>/img/twitter_logo.png" />
+                <img src="<?php echo get_template_directory_uri() ?>/img/linkedin_logo.png" />
+            </span>
         </div>
     </div>
 </div>
--- wp-content/themes/influencemonk/header.php
+++ wp-content/themes/influencemonk/header.php
@@ -13,21 +13,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<nav class="navbar">
-    <div class="container">
-        <div class="navbar-header">
-            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="<?php echo get_home_url(); ?> "> <span>influence</span>monk </a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse navbar-right">
+<nav class="navbar navbar-fixed-top">
+    <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo get_home_url(); ?> "> <span>influence</span>monk </a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse navbar-right">
 
-            <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => 'false', 'menu_class' => 'nav navbar-nav navbar-right', 'walker' => new Walker_Nav_Primary)); ?>
+        <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => 'false', 'menu_class' => 'nav navbar-nav navbar-right', 'walker' => new Walker_Nav_Primary)); ?>
 
-        </div>
     </div>
 </nav>
