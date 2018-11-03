6d9e5fc43f66fd222e3afa604a8e78b7446482ac
Sagnik Paul <sagnik.paul@nowfloats.com>
2018-11-04 04:00:45 +0530

    updated default page, updated sql file

--- wp-content/themes/influencemonk/page.php
+++ wp-content/themes/influencemonk/page.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 
-<?php
-if (have_posts()):
-    while (have_posts()): the_post(); ?>
+<div class="page">
 
-        <h1><?php the_title(); ?></h1>
+    <?php
+    if (have_posts()):
+        while (have_posts()): the_post(); ?>
 
-        <p><?php the_content(); ?></p>
+            <h1><?php the_title(); ?></h1>
 
-    <?php endwhile;
-endif;
-?>
+            <p><?php the_content(); ?></p>
+
+        <?php endwhile;
+    endif;
+    ?>
+
+</div>
 
 <?php get_footer(); ?>
