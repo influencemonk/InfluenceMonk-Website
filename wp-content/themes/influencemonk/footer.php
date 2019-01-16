2f85d1d28663382c0f17b50ff34b9073de5301ea
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-01-16 23:05:58 +0530

    Modal updated

--- wp-content/themes/influencemonk/footer.php
+++ wp-content/themes/influencemonk/footer.php
@@ -1,3 +1,100 @@
+<?php
+
+$indianStates = ['AR' => 'Arunachal Pradesh',
+'AR' => 'Arunachal Pradesh',
+'AS' => 'Assam',
+'BR' => 'Bihar',
+'CT' => 'Chhattisgarh',
+'GA' => 'Goa',
+'GJ' => 'Gujarat',
+'HR' => 'Haryana',
+'HP' => 'Himachal Pradesh',
+'JK' => 'Jammu and Kashmir',
+'JH' => 'Jharkhand',
+'KA' => 'Karnataka',
+'KL' => 'Kerala',
+'MP' => 'Madhya Pradesh',
+'MH' => 'Maharashtra',
+'MN' => 'Manipur',
+'ML' => 'Meghalaya',
+'MZ' => 'Mizoram',
+'NL' => 'Nagaland',
+'OR' => 'Odisha',
+'PB' => 'Punjab',
+'RJ' => 'Rajasthan',
+'SK' => 'Sikkim',
+'TN' => 'Tamil Nadu',
+'TG' => 'Telangana',
+'TR' => 'Tripura',
+'UP' => 'Uttar Pradesh',
+'UT' => 'Uttarakhand',
+'WB' => 'West Bengal',
+'AN' => 'Andaman and Nicobar Islands',
+'CH' => 'Chandigarh',
+'DN' => 'Dadra and Nagar Haveli',
+'DD' => 'Daman and Diu',
+'LD' => 'Lakshadweep',
+'DL' => 'National Capital Territory of Delhi',
+'PY' => 'Puducherry'];
+
+$generes = [];
+
+$generes[]  = "Fashion";
+$generes[]  = "Food";
+$generes[]  = "Travel";
+$generes[]  = "Lifestyle";
+$generes[]  = "Makeup";
+$generes[]  = "Fitness";
+$generes[]  = "Others";
+
+
+?>
+
+<div class="modal fade" id="getstarted" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Let's get Started</h4>
+            </div>
+            <div class="modal-body">
+                <div class="modal-formgroup">
+                    <label>Instagram Id</label>
+                    <input type="text" name="user-insta-id" class="form-control">
+                </div>
+                <div class="modal-formgroup">
+                  <select name="user-location-option" class="form-control" >
+              			<option value="all">Select your location</option>
+              			<?php
+              			foreach ($indianStates as $state){ ?>
+              				<option><?php echo $state; ?></option>
+              			<?php
+              			}
+              			?>
+              		</select>
+                </div>
+                <div class="modal-formgroup">
+                  <select name="user-generes-option" class="form-control">
+                    <option>Select your genre</option>
+                    <?php
+                    foreach ($generes as $genre){ ?>
+                      <option><?php echo $genre; ?></option>
+                    <?php
+                    }
+                    ?>
+                  </select>
+                </div>
+                <p class="message hide"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn">Submit</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
 <div class="footer">
     <div class="row">
         <div class="col-xs-12 col-sm-4 title">
